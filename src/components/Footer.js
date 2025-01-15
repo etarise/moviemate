04cc617c1d4f97f52acbbb75b96b79671b06fb00
Evans Tarise <73929569+etarise@users.noreply.github.com>
2025-01-15 14:02:09 +0000

    Initial Deployment

---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+export const Footer = () => {
+  var currentTime = new Date();
+  return (
+    <>
+      <footer className="bg-white  shadow m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <Link href="/" className="hover:underline mr-10">
+              © {currentTime.getFullYear()} MovieMate by Evans Tarise
+            </Link>
+            All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a
+                href="https://evans-tarise-portfolio-website.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline me-4 md:me-6"
+              >
+                My Website
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/evans-tarise"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline me-4 md:me-6"
+              >
+                LinkendIn
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://github.com/etarise"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
+  );
+};
---
+++ src/pages/Search.js
@@ -0,0 +1,31 @@
+import { MovieCard } from "../components/MovieCard";
+import { useFetch } from "../Hooks/useFetch";
+import { useSearchParams } from "react-router-dom";
+import { useTitle } from "../Hooks/useTitle";
+
+export const Search = ({ apiPath }) => {
+  const [searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data: movies } = useFetch(apiPath, queryTerm);
+
+  useTitle(`Search results for ${queryTerm} / Moviemate`);
+
+  return (
+    <main>
+      <section>
+        <p className="text-3xl text-gray-700 dark:text-white">
+          {movies.length === 0
+            ? `No result found for '${queryTerm}'`
+            : `Result for '${queryTerm}'`}
+        </p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
